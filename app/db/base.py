b4929a1aed206d199507aae7e2aa70503cf9d8c2
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-07-31 15:07:15 +0530

    new commits

--- app/db/base.py
+++ app/db/base.py
@@ -1,13 +1,8 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column,String
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 # here we will create a declarative base class that our models will inherit from 
-class base(DeclarativeBase):
+class Base(DeclarativeBase):
     pass
-class Users(base):
-    _Tablename_ = "user-info"
-    id:Mapped[int] = mapped_column(primary_key=True)
-    name:Mapped[str]= mapped_column(String(30),nullable= False)
-    email:Mapped[str]= mapped_column(unique=True)
 
 
---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from app.db.base import Base
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import String
+class Users(Base):
+    __tablename__ = "user-info"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str]= mapped_column(String(30),nullable= False)
+    email:Mapped[str]= mapped_column(String(255),unique=True,nullable=False)
+    def __repr__(self)->str:
+        return f"User(id= {self.id!r},name={self.name!r},email={self.email!r})"
