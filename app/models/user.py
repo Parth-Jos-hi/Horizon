31fecf1c64e756a2903cd88656c2b97abd371fa4
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-07-31 15:13:06 +0530

    change

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from app.db.base import Base
 from sqlalchemy.orm import Mapped,mapped_column
 from sqlalchemy import String
 class Users(Base):
-    __tablename__ = "user-info"
+    __tablename__ = "user"
     id:Mapped[int] = mapped_column(primary_key=True)
     name:Mapped[str]= mapped_column(String(30),nullable= False)
     email:Mapped[str]= mapped_column(String(255),unique=True,nullable=False)
