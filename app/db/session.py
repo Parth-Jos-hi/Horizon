321dc97b6ff7ac963bff88deddd70b8a4ce18dcf
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-01 12:08:11 +0530

    add Declerative_Base import to session.py and define SessionLocal

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,Declerative_Base
 from app.config import settings
 
 ## this will create a connection b/w the sql and the database 
 engine = create_engine(
     settings.DATABASE_URL
     ,echo = True
+)
+SessionLocal = sessionmaker(
+    ## bind tell the session maker that which engine to use for session maker
+    bind = engine,
+    ## it will not auto changes the commits after each operation
+    ## only after session.commit it will commit the changes
+    autocommit = False,
+    ## it will take the new query and jot it down in the temporary memory till we comming session.commit() 
+    # but it will be shown in the database when we search anything
+    autoflush =True
 )
 
