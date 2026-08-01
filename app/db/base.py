4702abb85ed97f45376c2c0571e83770e70a5cc8
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-01 12:33:17 +0530

    new chagnes modified

--- app/db/base.py
+++ app/db/base.py
@@ -1,6 +1,4 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
 # here we will create a declarative base class that our models will inherit from 
 class Base(DeclarativeBase):
     pass
--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,autocommit
+from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 ## this will create a connection b/w the sql and the database 
@@ -10,9 +10,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(
     ## bind tell the session maker that which engine to use for session maker
     bind = engine,
-    ## it will not auto changes the commits after each operation
-    ## only after session.commit it will commit the changes
-    autocommit = False,
+    # ## it will not auto changes the commits after each operation
+    # ## only after session.commit it will commit the changes
+    # autocommit = False,
+    ## new version of sql v2 the session is already in autocommit  = false so no need for this 
     ## it will take the new query and jot it down in the temporary memory till we comming session.commit() 
     # but it will be shown in the database when we search anything
     autoflush =True
