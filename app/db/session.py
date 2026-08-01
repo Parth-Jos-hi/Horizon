ac1e22464c12a1946937fa55c3267c94bcb1f178
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-01 11:02:03 +0530

    ###

--- app/db/session.py
+++ app/db/session.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+## this will create a connection b/w the sql and the database 
 engine = create_engine(
     "postgresql+psycopg2://postgres:Imparth@12_12@db.fhsvahlkqpxyzrylrcna.supabase.co:5432/postgres"
     ,echo = True
--- app/models/user.py
+++ app/models/user.py
@@ -1,23 +1,36 @@
-from app.db.base import Base
-from sqlalchemy.orm import Mapped,mapped_column
-from sqlalchemy import String
+# app/db/models/user.py
 import enum
-from sqlalchemy import Enum
-from sqlalchemy.types import UUID
 import uuid
-import datetime
-class roles(enum.Enum):
-    admin = "admin"
-    user = "user"
-class Users(Base):
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Enum, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from app.db.base import Base
+class UserRole(enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+class User(Base):
     __tablename__ = "users"
-    id:Mapped[int] = mapped_column(primary_key=True,as_uuid = True)
-    name:Mapped[str]= mapped_column(String(30),nullable= False)
-    email:Mapped[str]= mapped_column(String(255),unique=True,nullable=False)
-    password_hash:Mapped[str]= mapped_column(String(255),nullable = False)
-    role:Mapped[roles] = mapped_column(Enum(roles),nullable = False,default = roles.user)
-    created_at:Mapped[datetime.datetime]=mapped_column(default = datetime.datetime.now)
-    updated_at:Mapped[datetime.datetime.now] = mapped_column(default = datetime.datetime.now)
-    def __repr__(self)->str:
-        return f"User(id= {self.id!r},name={self.name!r},email={self.email!r})"
-    
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, server_default=func.uuid_generate_v4()
+    )
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole), nullable=False, default=UserRole.USER
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    def __repr__(self) -> str:
+        return (
+            f"User(id={self.id!r}, name={self.name!r}, email={self.email!r}, "
+            f"role={self.role!r}, created_at={self.created_at!r})"
+        )
