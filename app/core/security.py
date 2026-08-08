778d3f111cdd6a8e989bc95ed4af4f06f94a8dd4
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-08 16:40:41 +0530

    add password hashing and verification functions using bcrypt

--- app/core/security.py
+++ app/core/security.py
@@ -1,1 +1,11 @@
-from passlib.hash import bycrypt
+from passlib.hash import bycrypt 
+from passlib.context import CryptContext
+pass_context = CryptContext(schemes=["bycrypt"],deprecatted="auto")
+def hash_password(password:str)  -> str:
+    '''
+    it hashes the plain password using bycrypt
+    the resultant string will result algorithm,salt and hash.
+    '''
+    return pass_context.hash(password)
+def verify_password(plain_password:str,hashed_password:str)->bool:
+    return pass_context.verify(plain_password,hashed_password)
