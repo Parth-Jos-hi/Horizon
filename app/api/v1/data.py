a769d2b9d7902d5d58adef2724c9628e06e71a3f
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-20 00:12:58 +0530

    add initial market data endpoint with optional parameters

---
+++ app/api/v1/data.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from app.models import market_data_points
+from typing import Optional
+app = FastAPI()
+@app.get("/data")
+def market_data(region:Optional[str]=None,sector:Optional[str]=None,
+                role_category=Optional[str]=None,metric_type=Enum):
+    
