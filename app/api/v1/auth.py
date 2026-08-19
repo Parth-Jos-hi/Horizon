c43e07cff0ceca381b494d096e1ef2acc11989fc
Parth Joshi <190162247+Parth-Jos-hi@users.noreply.github.com>
2026-08-19 23:53:33 +0530

    fix login endpoint route and add user registration functionality

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from pydantic import SecretStr
 from app.services.auth_service import register_user
+from app.dependencies import get_current_user
+from sqlalchemy.orm import Session
 app = FastAPI()
-@app.post("/login")
+@app.post("/register")
 def register_user(display_name:str,password:SecretStr):
     return register_user(display_name, password)
+@app.get("login")
+async def UserLogin(token:str,db:Session):
+    return get_current_user(token,db)
